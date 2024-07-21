88f6f0cd52ef2a106c14734819da0f7280cd8da1
prashob <vp.prashob@gmail.com>
2024-07-21 22:15:14 +0200

    movie-app-v1

---
+++ movie-app-backend/cmd/api/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"backend/internal/handlers"
+	"backend/internal/jwt"
+	"backend/internal/repository/dbrepo"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+const port = 8080
+
+func main() {
+	// set application config
+	var app handlers.Application
+
+	// read from command line
+	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5432 user=postgres password=postgres dbname=movies sslmode=disable timezone=UTC connect_timeout=5", "Postgres connection string")
+	flag.StringVar(&app.JWTSecret, "jwt-secret", "verysecret", "signing secret")
+	flag.StringVar(&app.JWTIssuer, "jwt-issuer", "example.com", "signing issuer")
+	flag.StringVar(&app.JWTAudience, "jwt-audience", "example.com", "signing audience")
+	flag.StringVar(&app.CookieDomain, "cookie-domain", "localhost", "cookie domain")
+	flag.StringVar(&app.Domain, "domain", "example.com", "domain")
+	flag.StringVar(&app.APIKey, "api-key", "b41447e6319d1cd467306735632ba733", "api key")
+	flag.Parse()
+
+	// connect to the database
+	conn, err := app.ConnectToDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	app.DB = &dbrepo.PostgresDBRepo{DB: conn}
+	defer app.DB.Connection().Close()
+
+	
+
+	app.Auth = jwt.Auth{
+		Issuer:        app.JWTIssuer,
+		Audience:      app.JWTAudience,
+		Secret:        app.JWTSecret,
+		TokenExpiry:   time.Minute * 15,
+		RefreshExpiry: time.Hour * 24,
+		CookiePath:    "/",
+		CookieName:    "__Host-refresh_token",
+		CookieDomain:  app.CookieDomain,
+	}
+
+	log.Println("Starting application on port", port)
+
+	// start a web server
+	err = http.ListenAndServe(fmt.Sprintf(":%d", port), app.Routes())
+	if err != nil {
+		log.Fatal(err)
+	}
+}
